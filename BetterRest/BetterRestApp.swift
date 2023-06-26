8b00c57bda05b187d347b72c6b7e4e8aa066c7e7
Diego Perdomo <diegoperdomo94@gmail.com>
2023-06-26 16:56:46 -0500

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Diego Perdomo on 26/6/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
